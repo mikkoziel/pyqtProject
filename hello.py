7ca3e62080782d82f46587e951f17fe81859d1d5
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-10-05 12:10:29 +0200

    QForm

--- hello.py
+++ hello.py
@@ -1,21 +1,18 @@
 import sys
 
-# 1. Import `QApplication` and all the required widgets
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QFormLayout
+from PyQt5.QtWidgets import QLineEdit
 from PyQt5.QtWidgets import QWidget
 
 app = QApplication(sys.argv)
-
 window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
-
-# 4. Show your application's GUI
+window.setWindowTitle('QFormLayout')
+layout = QFormLayout()
+layout.addRow('Name:', QLineEdit())
+layout.addRow('Age:', QLineEdit())
+layout.addRow('Job:', QLineEdit())
+layout.addRow('Hobbies:', QLineEdit())
+window.setLayout(layout)
 window.show()
-
-# 5. Run your application's event loop (or main loop)
 sys.exit(app.exec_())
