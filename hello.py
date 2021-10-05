68b5650f212be75b7acf8f4d7f8a004d047b44e3
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-10-05 12:22:24 +0200

    MainWindow

--- hello.py
+++ hello.py
@@ -1,21 +1,40 @@
 import sys
 
-# 1. Import `QApplication` and all the required widgets
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
-from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QStatusBar
+from PyQt5.QtWidgets import QToolBar
 
-app = QApplication(sys.argv)
 
-window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
+class Window(QMainWindow):
+    """Main Window."""
+    def __init__(self, parent=None):
+        """Initializer."""
+        super().__init__(parent)
+        self.setWindowTitle('QMainWindow')
+        self.setCentralWidget(QLabel("I'm the Central Widget"))
+        self._createMenu()
+        self._createToolBar()
+        self._createStatusBar()
 
-# 4. Show your application's GUI
-window.show()
+    def _createMenu(self):
+        self.menu = self.menuBar().addMenu("&Menu")
+        self.menu.addAction('&Exit', self.close)
 
-# 5. Run your application's event loop (or main loop)
-sys.exit(app.exec_())
+    def _createToolBar(self):
+        tools = QToolBar()
+        self.addToolBar(tools)
+        tools.addAction('Exit', self.close)
+
+    def _createStatusBar(self):
+        status = QStatusBar()
+        status.showMessage("I'm the Status Bar")
+        self.setStatusBar(status)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec_())
