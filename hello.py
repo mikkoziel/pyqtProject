b662d82077026b51da79b8e09c2dbe20635f3a4a
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-10-05 12:07:12 +0200

    Grid

--- hello.py
+++ hello.py
@@ -1,21 +1,22 @@
 import sys
 
-# 1. Import `QApplication` and all the required widgets
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QGridLayout
+from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QWidget
 
 app = QApplication(sys.argv)
-
 window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
-
-# 4. Show your application's GUI
+window.setWindowTitle('QGridLayout')
+layout = QGridLayout()
+layout.addWidget(QPushButton('Button (0, 0)'), 0, 0)
+layout.addWidget(QPushButton('Button (0, 1)'), 0, 1)
+layout.addWidget(QPushButton('Button (0, 2)'), 0, 2)
+layout.addWidget(QPushButton('Button (1, 0)'), 1, 0)
+layout.addWidget(QPushButton('Button (1, 1)'), 1, 1)
+layout.addWidget(QPushButton('Button (1, 2)'), 1, 2)
+layout.addWidget(QPushButton('Button (2, 0)'), 2, 0)
+layout.addWidget(QPushButton('Button (2, 1) + 2 Columns Span'), 2, 1, 1, 2)
+window.setLayout(layout)
 window.show()
-
-# 5. Run your application's event loop (or main loop)
 sys.exit(app.exec_())
