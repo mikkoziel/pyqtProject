12b1c6ab60ba244adde900cd4b258152c381334d
Mikołaj Kozieł <mkoziel045@gmail.com>
2021-10-05 12:03:47 +0200

    HBox sample

--- hello.py
+++ hello.py
@@ -1,21 +1,17 @@
 import sys
 
-# 1. Import `QApplication` and all the required widgets
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QHBoxLayout
+from PyQt5.QtWidgets import QPushButton
 from PyQt5.QtWidgets import QWidget
 
 app = QApplication(sys.argv)
-
 window = QWidget()
-window.setWindowTitle('PyQt5 App')
-window.setGeometry(100, 100, 280, 80)
-window.move(60, 15)
-helloMsg = QLabel('<h1>Hello World!</h1>', parent=window)
-helloMsg.move(60, 15)
-
-# 4. Show your application's GUI
+window.setWindowTitle('QHBoxLayout')
+layout = QHBoxLayout()
+layout.addWidget(QPushButton('Left'))
+layout.addWidget(QPushButton('Center'))
+layout.addWidget(QPushButton('Right'))
+window.setLayout(layout)
 window.show()
-
-# 5. Run your application's event loop (or main loop)
 sys.exit(app.exec_())
